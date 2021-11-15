a84bd18a1c21b0f48807d3007063fdeeded0ece1
Moisés Mariano <moisesduartem@gmail.com>
2021-11-14 21:58:35 -0300

    feat: configure ef core (in memory)

---
+++ AtTime/AtTime.Infra/DependencyResolver.cs
@@ -0,0 +1,25 @@
+﻿using AtTime.Infra.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AtTime.Infra
+{
+    public static class DependencyResolver
+    {
+        public static void AddConfiguration(this IServiceCollection services)
+        {
+            AddDependencyInjection(services);
+            AddEntityFrameworkCore(services);
+        }
+
+        private static void AddDependencyInjection(IServiceCollection services)
+        {
+            services.AddScoped<DataContext, DataContext>();
+        }
+        
+        private static void AddEntityFrameworkCore(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("AtTimeDB"));
+        }
+    }
+}
