d0ea1b732edeed9ee4fdfdb4c7acdaace78bd400
Moisés Mariano <moisesduartem@gmail.com>
2021-11-14 21:37:25 -0300

    feat: create api projects & user model

---
+++ AtTime/AtTime.Core/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtTime.Core.Models
+{
+    public sealed class User
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [MinLength(3)]
+        [MaxLength(60)]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+    }
+}
